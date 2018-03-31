68440283ac66a0eaed0bd6f4d09e474220016c11
Kévin Baubet <contact@kevinbaubet.fr>
2018-03-31 19:46:03 +0200

    Add destroy method and onChange callback

--- README.md
+++ README.md
@@ -9,10 +9,18 @@ Ce script permet d'afficher aléatoirement des éléments.
 
 ## Options
 
-| Option      | Type    | Valeur par défaut | Description                                      |
-|-------------|---------|-------------------|--------------------------------------------------|
-| activeClass | string  | 'is-active'       | Classe sur l'élément enfant quand il est affiché |
-| duration    | integer | 3000              | Temps en ms entre chaque changement d'affichage  |
-| loop        | boolean | true              | L'affiche des éléments tourne en boucle          |
-| shuffleLoop | boolean | false             | Modifier l'ordre d'affichage à chaque boucle     |
-| minItems    | integer | 1                 | Nombre d'élément minimum à laisser afficher      |
+| Option      | Type     | Valeur par défaut | Description                                      |
+|-------------|----------|-------------------|--------------------------------------------------|
+| activeClass | string   | 'is-active'       | Classe sur l'élément enfant quand il est affiché |
+| duration    | integer  | 3000              | Temps en ms entre chaque changement d'affichage  |
+| loop        | boolean  | true              | L'affiche des éléments tourne en boucle          |
+| shuffleLoop | boolean  | true              | Modifier l'ordre d'affichage à chaque boucle     |
+| minItems    | integer  | 1                 | Nombre d'élément minimum à laisser afficher      |
+| onChange    | function | undefined         | Callback à chaque changement d'état d'un élément |
+
+
+## Méthodes
+
+| Méthode       | Arguments      | Description                                                        |
+|---------------|----------------|--------------------------------------------------------------------|
+| destroy       | -              | Détruit le fonctionnement de RandomItems pour revenir à la normale |
--- src/randomitems.js
+++ src/randomitems.js
@@ -1,95 +1,115 @@
-/**
- * RandomItems
- *
- * Permet d'afficher aléatoire des éléments
- *
- * @version 1.0 (19/11/2015)
- */
-(function($) {
+(function ($) {
     'use strict';
 
-    $.RandomItems = function(parent, options) {
+    $.RandomItems = function (container, options) {
         // Éléments
-        this.elements = {
-            parent: parent
-        };
+        this.elements = {};
+        this.elements.container = container,
+        this.elements.items = this.elements.container.children();
 
         // Config
         $.extend((this.settings = {}), $.RandomItems.defaults, options);
 
         // Variables par défaut
-        this.len = 0;
         this.order = [];
+        this.loop;
+        this.timers = {};
 
         // Init
-        this.init();
+        return this.init();
     };
 
     $.RandomItems.defaults = {
         activeClass: 'is-active',
         duration: 3000,
         loop: true,
-        shuffleLoop: false,
-        minItems: 1
+        shuffleLoop: true,
+        minItems: 1,
+        onChange: undefined
     };
 
     $.RandomItems.prototype = {
         /**
          * Initialisation
          */
-        init: function() {
+        init: function () {
             var self = this;
 
-            // Liste des éléments
-            self.elements.items = self.elements.parent.children();
-            self.len = self.elements.items.length;
+            if (self.elements.items.length) {
+                // Définition de l'ordre
+                var i = 0;
+                for (i; i < self.elements.items.length; i++) {
+                    self.order[i] = i;
+                }
 
-            // Définition de l'ordre
+                // Aléatoire
+                self.shuffleOrder();
+                self.applyOrder();
+
+                // Si c'est en mode loop, on répète l'ordre aléatoire
+                if (self.settings.loop) {
+                    self.loop = setInterval(function () {
+                        if (self.settings.shuffleLoop) {
+                            self.shuffleOrder();
+                        }
+                        self.applyOrder();
+                    }, self.elements.items.length * self.settings.duration);
+                }
+            }
+
+            return self;
+        },
+
+        /**
+         * Stop le fonctionnement de RandomItems
+         */
+        destroy: function () {
+            // Stop loop
+            if (this.settings.loop) {
+                clearInterval(this.loop);
+            }
+
+            // Stop timeout
             var i = 0;
-            for (i; i < self.len; i++) {
-                self.order[i] = i;
-            }
-
-            // Aléatoire
-            self.shuffleOrder();
-            self.applyOrder();
-
-            // Si c'est en mode loop, on répère l'ordre aléatoire
-            if (self.settings.loop) {
-                setInterval(function() {
-                    if (self.settings.shuffleLoop) {
-                        self.shuffleOrder();
+            for (i; i < this.elements.items.length; i++) {
+                if (this.timers[i] !== undefined) {
+                    if (this.timers[i].add !== undefined) {
+                        clearTimeout(this.timers[i].add);
                     }
-                    self.applyOrder();
-                }, self.len * self.settings.duration)
+                    if (this.timers[i].remove !== undefined) {
+                        clearTimeout(this.timers[i].remove);
+                    }
+                }
             }
+
+            // Remove classes
+            this.elements.items.removeClass(this.settings.activeClass);
+
+            return this;
         },
 
         /**
          * Créer l'ordre aléatoire
          */
-        shuffleOrder: function() {
-            var lastId = this.len;
+        shuffleOrder: function () {
+            this.order.sort(function() {
+                return .5 - Math.random();
+            });
 
-            while (lastId > 0) {
-                var randId = Math.floor(Math.random() * lastId);
-                var lastElem = this.order[--lastId];
-
-                this.order[lastId] = this.order[randId];
-                this.order[randId] = lastElem;
-            }
+            return this;
         },
 
         /**
          * Applique l'ordre aléatoire aux éléments
          */
-        applyOrder: function() {
+        applyOrder: function () {
             var self = this;
 
-            $.each(this.order, function(i, item) {
-                var item = self.elements.items.eq(item);
-                self.applyItem(i, item);
+            $.each(self.order, function (i, item) {
+                self.applyItem(i, self.elements.items.eq(item));
             });
+
+            return self;
         },
 
         /**
@@ -98,20 +118,43 @@
          * @param  int    i    Index de l'ordre
          * @param  object item Element jquery
          */
-        applyItem: function(i, item) {
+        applyItem: function (i, item) {
             var self = this;
+            self.timers[i] = {};
 
-            setTimeout(function() {
+            self.timers[i].add = setTimeout(function () {
                 item.addClass(self.settings.activeClass);
 
-                setTimeout(function() {
+                // User callback
+                if (self.settings.onChange !== undefined) {
+                    self.settings.onChange.call({
+                        randomItems: self,
+                        item: item,
+                        active: true,
+                        index: i
+                    });
+                }
+
+                self.timers[i].remove = setTimeout(function () {
                     item.removeClass(self.settings.activeClass);
+
+                    // User callback
+                    if (self.settings.onChange !== undefined) {
+                        self.settings.onChange.call({
+                            randomItems: self,
+                            item: item,
+                            active: false,
+                            index: i
+                        });
+                    }
                 }, self.settings.minItems * self.settings.duration);
             }, i * self.settings.duration);
+
+            return self;
         }
     };
 
-    $.fn.randomItems = function(options) {
+    $.fn.randomItems = function (options) {
         return new $.RandomItems($(this), options);
     };
 })(jQuery);
